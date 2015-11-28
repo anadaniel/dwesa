22c38e22e8f23d824ae44690b57d25e63c59b952
Ana Daniel <anadanield@gmail.com>
2015-11-28 14:31:17 +0100

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+route = Route.create(name: "Willowvale - Dwesa")
+Checkpoint.create([
+    { name: "Willowvale", weight: 5.0, route: route, position: 1 },
+    { name: "Church", weight: 5.0, route: route, position: 2 },
+    { name: "School", weight: 5.0, route: route, position: 3 },
+    { name: "Bridge", weight: 5.0, route: route, position: 4 },
+    { name: "Dwesa", weight: 5.0, route: route, position: 5 }
+  ])
