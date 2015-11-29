9f943c1f964b6170925c2d189d6f05468e48731d
ferraricharles <charles.cjf@gmail.com>
2015-11-29 12:03:29 +0100

    Starting frontend

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= modernizr
 //= require_tree .
+
--- app/assets/javascripts/checkpoints.js
+++ app/assets/javascripts/checkpoints.js
@@ -1,2 +1,49 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+jQuery(document).ready(function($){
+	
+	$('#add-event-button').click(function(){
+
+
+        $('.page').hide('hide');
+        
+       	$(location).attr('href','#page1');
+
+
+    });
+    $(window).on("hashchange", function () {
+    	var location  = top.location.hash;
+    	alert(location);
+    	switch(location){
+    		case "page1":
+    			$('#page1').show();
+    			breack;
+
+    	}
+    })
+
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+});
