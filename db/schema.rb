3655328085c28bf146e1e017877419f677b39355
djbb7 <danielbruzual@gmail.com>
2015-11-29 11:32:37 +0100

    add position field to checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128142912) do
+ActiveRecord::Schema.define(version: 20151129102417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20151128142912) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "route_id"
-    t.float    "km"
+    t.integer  "position"
   end
 
   create_table "notices", force: :cascade do |t|
